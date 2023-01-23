f0af17785fe4c9e5c231c0bca01d56ecb70e5197
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-01-23 16:40:53 -0300

    Atividade SpringController

---
+++ projetospringController.java
@@ -0,0 +1,30 @@
+package org.generation.projetospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // informa que a classe é um controller
+@RequestMapping("/index")//Informa o endereço web
+
+
+public class projetospringController {
+	
+		@GetMapping // pega informação do metodo e exibe
+		public String hello() {
+			return ("Hello Wolrd! Seja, bem vinda ao Spring");
+		}
+		
+
+		@RequestMapping("/bsm")
+
+	public String bsm() {
+			return ("Bsm's da Generation Brasil!");
+		}
+		
+		@RequestMapping("/bsm/obj")
+		
+			public String obj() {
+			return ("Objetivo de aprendizagem da semana...");
+		}
+}
